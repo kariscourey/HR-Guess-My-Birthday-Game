362bfb084fd24036b776deafd3bd22bb9c5c85f4
karis courey <kariscourey@gmail.com>
2022-08-17 15:04:57 -0400

    Add game.py

---
+++ game.py
@@ -0,0 +1,38 @@
+from random import randint
+
+# Initialize variables
+
+rounds = 5
+answer = ""
+
+# Obtain user name
+name = input("Hi! What is your name? ")
+
+# Evaluate round
+for i in range(rounds):
+
+    month = randint(1, 12)
+    year = randint(1924, 2004)
+
+    if answer != "Y":
+
+        # Report guess and obtain user answer
+        print("Guess " + str(i + 1) + ": " + name + ", you were born in " + str(month) + " / " + str(year) + ".")
+        answer = input("Y/N? ")
+        answer = answer.upper()
+
+        # Evaluate answer in loop
+        if i != (rounds - 1):
+            if answer == "N":
+                print("Drat! Lemme try again.")
+            elif answer != "Y":
+                print("Invalid input!")
+
+    else:
+        exit
+
+# Evaluate answer after loop
+if answer == "Y":
+    print("I knew it!")
+else:
+    print("I have other things to do. Goodbye.")
